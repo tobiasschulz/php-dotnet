1267fba1cf88fc686e06b069cf7d021463debe2a
Tobias Schulz <tobiasschulz.dev@outlook.com>
2018-10-30 11:08:10 +0100

    Update PHP.Dotnet/PHP.Interpreter/Program.cs

--- PHP.Dotnet/PHP.Interpreter/Program.cs
+++ PHP.Dotnet/PHP.Interpreter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PHP.Interpreter
 {
@@ -10,12 +11,23 @@ namespace PHP.Interpreter
 
             Context context = new Context ();
 
-            context.AddDirectory (@"C:\tobias.schulz\GIT\dfd_posdb\src");
-            context.AddDirectory (@"C:\tobias.schulz\GIT\dk_framework\src");
-            context.AddDirectory (@"C:\tobias.schulz\GIT\dk_rm\src");
-            context.AddDirectory (@"C:\tobias.schulz\GIT\dk_app\src");
+            if (Directory.Exists ("/Users/tobias/di" + "gi" + "tal" + "kr" + "aft/git/d" + @"k_fr" + @"am" + @"ew" + @"or" + @"k / "))
+            {
+                context.AddDirectory (@"/Users/tobias/di" + "gi" + "tal" + "kr" + "aft/git/df" + @"d_po" + @"sdb/src");
+                context.AddDirectory (@"/Users/tobias/di" + "gi" + "tal" + "kr" + "aft/git/dk_" + @"fra" + @"mewo" + @"rk/src");
+                context.AddDirectory (@"/Users/tobias/di" + "gi" + "tal" + "kr" + "aft/git/dk_" + @"r" + @"m/src");
+                context.AddDirectory (@"/Users/tobias/di" + "gi" + "tal" + "kr" + "aft/git/dk_" + @"ap" + @"p/src");
 
-            context.RunFile (@"C:\tobias.schulz\GIT\dk_framework\src\public\index.php");
+                context.RunFile (@"/Users/tobias/di" + "gi" + "tal" + "kr" + "aft/src/public/index.php");
+            }
+            else {
+                context.AddDirectory (@"C:\tobias.schulz\GIT\df" + @"d_po" + @"sdb\src");
+                context.AddDirectory (@"C:\tobias.schulz\GIT\dk_" + @"fra" + @"mewo" + @"rk\src");
+                context.AddDirectory (@"C:\tobias.schulz\GIT\dk_" + @"r" + @"m\src");
+                context.AddDirectory (@"C:\tobias.schulz\GIT\dk_" + @"ap" + @"p\src");
+
+                context.RunFile (@"C:\tobias.schulz\GIT\d" + @"k_fr" + @"ame" + @"w" + @"ork\src\public\index.php");
+            }
 
             Console.ReadLine ();
         }
